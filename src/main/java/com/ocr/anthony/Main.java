d7e432500e9ef976ea0633321ea063fd75065a33
christophe-alhalel <41323753+christophe-alhalel@users.noreply.github.com>
2018-07-17 11:10:22 +0200

    Add comments to explain main objective

--- src/main/java/com/ocr/anthony/Main.java
+++ src/main/java/com/ocr/anthony/Main.java
@@ -2,6 +2,9 @@ package com.ocr.anthony;
 
 public class Main {
 
+    /*
+    Programme principal tutoriel pour gérer des commandes de menus
+    */
     public static void main(String[] args) {
         Order order = new Order();
         order.runMenus();
